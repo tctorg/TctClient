8641becad8b1c5f6e09dc8bd22dd7b33242b374c
Zorian <bbbird@outlook.com>
2015-06-23 20:52:04 +0800

    add forum db schema

--- TCT/WebService/src/main/SQL/CreateTables.sql
+++ TCT/WebService/src/main/SQL/CreateTables.sql
@@ -6,11 +6,27 @@ CREATE TABLE "user" (
 
 CREATE TABLE "events" (
   "id" serial NOT NULL,
-  "name" character(128) NOT NULL
+  "name" character(128) NOT NULL,
+  "seat_max" smallint NOT NULL DEFAULT '0'
 );
 
 CREATE TABLE "register" (
   "id" serial NOT NULL,
   "eid" integer NOT NULL,
   "uid" integer NOT NULL
+);
+
+CREATE TABLE "topic" (
+  "id" serial NOT NULL,
+  "uid" integer NOT NULL,
+  "time" timestamp NOT NULL,
+  "subject" character varying(512) NOT NULL,
+  "content" character varying(5120) NOT NULL
+);
+
+CREATE TABLE "comment" (
+  "id" serial NOT NULL,
+  "tid" integer NOT NULL,
+  "time" timestamp NOT NULL,
+  "comment" character varying(5120) NOT NULL
 );
