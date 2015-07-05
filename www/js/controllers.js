5741ce12bbe729d1e450deacb02d2163b3164d83
= <kaven.feng@autodesk.com>
2015-07-05 11:26:59 +0800

    landing to signin to events

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -29,13 +29,17 @@ angular.module('starter.controllers', [])
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
 
-
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
     $timeout(function() {
       $scope.closeLogin();
     }, 1000);
   };
+
+  $scope.doSignin = function(){
+    $state.go("app.events");
+  };
+
 })
 
 .controller('EventsCtrl', function($scope,Events) {
